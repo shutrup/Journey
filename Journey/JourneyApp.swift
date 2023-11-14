5bd881d5a51c1f0ea4055c898b9f424979e1edf4
shutrup <magagaraev95@mail.ru>
2023-11-14 09:35:33 +0300

    Initial Commit

---
+++ Journey/JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyApp.swift
+//  Journey
+//
+//  Created by Шарап Бамматов on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
