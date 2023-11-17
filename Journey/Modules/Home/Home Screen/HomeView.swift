509a1fdfea6b2b40ad05678cb14234863d1dd62a
shutrup <magagaraev95@mail.ru>
2023-11-17 17:56:35 +0300

    download all fonts and some icons for HomeView

---
+++ Journey/Modules/Home/Home Screen/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Journey
+//
+//  Created by Шарап Бамматов on 17.11.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
