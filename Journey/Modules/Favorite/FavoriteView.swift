1d5804764dd1aa90afa9ee4c16139db835529e5c
shutrup <magagaraev95@mail.ru>
2023-11-27 16:15:57 +0300

    create FavoriteView

---
+++ Journey/Modules/Favorite/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  Journey
+//
+//  Created by Шарап Бамматов on 27.11.2023.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
